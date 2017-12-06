3ac4a50f78e55697337ba8ab2fbd4d4d808b7cee
He Lin <oxxxxd@gmail.com>
2017-12-06 22:52:18 +0800

    store_header dropdown and some of cart.html layout

--- js/store_header.js
+++ js/store_header.js
@@ -1,10 +1,10 @@
-(()=>{
+(() => {
     ajax({
-        type:"get",
-        url:"store_header.html"
-    }).then(html =>{
+        type: "get",
+        url: "store_header.html"
+    }).then(html => {
         document.getElementById("store_header").innerHTML = html;
-        window.onscroll = ()=>{
+        window.onscroll = () => {
             var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
             var store_nav = document.getElementById("store_nav");
             if (scrollTop > 250) {
@@ -13,5 +13,23 @@
                 store_nav.className = "";
             }
         };
+
+        // 鼠标悬停下拉菜单功能
+        // var ul = document.querySelector("#store_nav>ul");
+        // ul.onmouseover = e => {
+        //     // console.log(e.target);
+        // };
+        // JQuery 实现尝试
+
+        $("#store_nav>ul>li").mouseover(function(e) {
+            if (e.target.nodeName == "LI") {
+                var $tar = $(e.target);
+                $tar.children(".dropdown").css("display", "block");
+            }
+        }).mouseleave(function() {
+            // console.log(this);
+            // console.log($(this).children(".dropdown"));
+            $(this).children(".dropdown[style]").css("display", "none");
+        });
     });
 })();
