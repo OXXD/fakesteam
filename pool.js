814d2bb0306dcdd449612895d138867ec65a7cfa
He Lin <oxxxxd@gmail.com>
2018-02-07 23:50:27 +0800

    add sinacloud mysql

--- pool.js
+++ pool.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'steam',
+    host: 'w.rdc.sae.sina.com.cn',
+    user: 'mkk3nj0w1n',
+    password: 'z3y2hmx35iklh1i3xjjx2z3x4j145xx0w10i1iim',
+    database: 'app_fakesteam',
     port: 3306,
     connectionLimit: 25
 });
